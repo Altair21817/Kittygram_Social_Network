7600dd696ffd748cf48e8982cec4a6c0e6db78dd
= <=>
2023-05-12 20:50:49 +0000

    Add media folder

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -89,14 +89,11 @@ USE_L10N = True
 
 USE_TZ = True
 
+STATIC_URL = '/static_backend/'
+STATIC_ROOT = BASE_DIR / 'static_backend'
 
-STATIC = 'static_backend'
-STATIC_URL = STATIC
-STATIC_ROOT = BASE_DIR / STATIC
-
-MEDIA = 'media'
-MEDIA_URL = MEDIA
-MEDIA_ROOT = BASE_DIR / MEDIA
+MEDIA_URL = '/media/'
+MEDIA_ROOT = '/var/www/kittygram/media/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
